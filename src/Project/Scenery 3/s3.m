93cc7add62746169d48c8f92eccdf46941118528
ATOMICXXPOWER <tancredi.bertolotti@gmail.com>
2026-05-15 16:11:50 +0200

    Refactor orbital elements section and add comments for clarity

--- src/Project/Scenery 3/s3.m
+++ src/Project/Scenery 3/s3.m
@@ -18,9 +18,52 @@ M = 1.0472E+12;                         % Mass of 3908 Nyx [kg]
 D = 1.00;                               % Diameter of 3908 Nyx [km]
 G = 6.67430e-20;                        % Gravitational constant [km^3/kg/s^2]
 mu = G*M;                               % Gravitational parameter of 3908 Nyx [km^3/s^2]
-a_nyx  = 1.927925 * AU;                 % Semi-major axis [km]
+    % Sphere of Influence of 3908 Nyx [km] %CHECK FOR VALIDITY
 
-v_escape = sqrt(2*mu/(D/2));            % Escape velocity from 3908 Nyx [km/s]
+%% 2. ORBITAL ELEMENTS OF 3908 NYX
+a = 1.927925;
+e_nyx = 0.459072;
+i_nyx = deg2rad(2.19);
+OM_nyx = deg2rad(261.19);
+om_nyx = deg2rad(126.66);
+M_nyx = 1.0472E+12;
+D_nyx = 1.00;
 
-r_SOI_nyx = a_nyx * (M/M_sun)^(2/5);    % Sphere of Influence of 3908 Nyx [km] %CHECK FOR VALIDITY
+a_nyx  = a * AU;                 % Semi-major axis [km]
+v_escape_nyx = sqrt(2*mu/(D_nyx/2));     % Escape velocity from 3908 Nyx [km/s]
+
+
+
+%% 3 Earth's orbital elements
+
+a_ea  = 1.4946e+08;                  % Semi-major axis [km]
+e_ea  = 0.016;                       % Eccentricity [-]
+i_ea  = 9.1920e-05;                  % Inclination [rad]
+OM_ea = 2.7847;                      % Longitude of Ascending Node [rad]
+om_ea = 5.2643;                      % Argument of Pericenter [rad]
+
+mu_ea = 398600.4418;             % Gravitational parameter of Earth [km^3/s^2]
+r_ea = 6371;                      % Radius of Earth [km]
+
+
+%% ORBITAL ELEMENTS OF PARKING EARTH ORBIT
+r_xf = -6103.007500;
+r_yf = 13604.661000;
+r_zf = 7991.352600;
+
+v_xf = -5.163000;
+v_yf = -3.151000;
+v_zf = 1.421000;
+
+
+rf = [r_xf; r_yf; r_zf];
+vf = [v_xf; v_yf; v_zf];
+
+
+[a_start, e_start, i_start, OM_start, om_start, th_start] = car2par(rf, vf, mu);
+
+
+%% FIRST HYPERBOLIC ESCAPE FROM EARTH
+% Vinf = %Vtransfer s2 - V earth (prendere da output s2)
+
 
